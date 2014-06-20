c03a32ff7558d3899bc37786e5849a0f5bf67b4c
Ben <benjaminwpeacock@gmail.com>
2014-06-19 20:47:36 -0600

    Create Event model

---
+++ db/migrate/20140620024644_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :address
+      t.date :endDate
+      t.string :imageThumbUrl
+      t.text :shortDescription
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
